7d7d4c6c4b2725ca20a115542e66f2598e55362a
Abhishek Arora <abhishekarora12@gmail.com>
2017-04-05 23:01:56 +0530

    Created message interaction module layout

--- app/src/main/java/com/example/naveen/EmotionApp/WriteNotesActivity.java
+++ app/src/main/java/com/example/naveen/EmotionApp/WriteNotesActivity.java
@@ -1,5 +1,6 @@
 package com.example.naveen.EmotionApp;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
@@ -8,21 +9,33 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
-
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 import java.io.File;
+
+import static android.content.ContentValues.TAG;
 
 public class WriteNotesActivity extends AppCompatActivity {
     String capturedImageFilePath = "";
+    NestedScrollView nestedScrollView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_write_notes);
         capturedImageFilePath  = MyCamera.takePictureAndSave(this);
     }
 
@@ -34,6 +47,46 @@ public class WriteNotesActivity extends AppCompatActivity {
         else{
             finish();
         }
+    }
+
+    protected void onClickButton(View view) {
+        // Add text views dynamically
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.interactBox);
+
+        // Add textview 1
+        TextView textView1 = new TextView(this);
+        LayoutParams layoutParamsLeft = new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        layoutParamsLeft.gravity = Gravity.LEFT;
+        textView1.setLayoutParams(layoutParamsLeft);
+        textView1.setText("programmatically created TextView1");
+        textView1.setBackgroundResource(R.drawable.bubble1);
+        //textView1.setBackgroundColor(0xff66ff66); // hex color 0xAARRGGBB
+        //textView1.setPadding(20, 20, 20, 20);// in pixels (left, top, right, bottom)
+        linearLayout.addView(textView1);
+
+        // Add textview 2
+        TextView textView2 = new TextView(this);
+        LayoutParams layoutParamsRight = new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        layoutParamsRight.gravity = Gravity.RIGHT;
+        //layoutParams.setMargins(10, 10, 10, 10); // (left, top, right, bottom)
+        textView2.setLayoutParams(layoutParamsRight);
+        textView2.setText("programmatically created TextView2");
+        textView2.setBackgroundResource(R.drawable.bubble2);
+        //textView2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        //textView2.setBackgroundColor(0xffffdbdb); // hex color 0xAARRGGBB
+        linearLayout.addView(textView2);
+
+        nestedScrollView = (NestedScrollView) this.findViewById(R.id.scroll);
+        //nestedScrollView.fullScroll(nestedScrollView.FOCUS_DOWN);
+        nestedScrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                nestedScrollView.fullScroll(nestedScrollView.FOCUS_DOWN);
+            }
+        });
+        Log.d(TAG, "onClickButton: After Click");
     }
 
     class ImageLoader extends AsyncTask<String, Void, Bitmap> {
